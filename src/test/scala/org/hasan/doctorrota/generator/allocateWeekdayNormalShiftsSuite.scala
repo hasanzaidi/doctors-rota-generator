091dd6f8f97bf741c7396437f7281db66a1db0f0
Hasan Zaidi <hasan.zaidi@rocketmail.com>
2020-06-06 18:24:30 +0100

    Add unit test.

---
+++ src/test/scala/org/hasan/doctorrota/generator/allocateWeekdayNormalShiftsSuite.scala
@@ -0,0 +1,35 @@
+package org.hasan.doctorrota.generator
+
+import java.time.LocalDate
+
+import org.hasan.doctorrota.allocateWeekdayNormalShifts
+import org.hasan.doctorrota.domain.DayType._
+import org.hasan.doctorrota.domain.Doctor
+import org.hasan.doctorrota.domain.Shift
+import org.hasan.doctorrota.domain.ShiftFactory
+import org.hasan.doctorrota.domain.ShiftType._
+import org.hasan.doctorrota.domain.WeeklyRota
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import scala.collection.mutable.ListBuffer
+
+class allocateWeekdayNormalShiftsSuite extends AnyFunSuite with Matchers {
+  test("can allocate normal weekday shifts") {
+    // Given:
+    val shift1 = ShiftFactory(NORMAL, WEEKDAY, LocalDate.of(2020, 6, 15))
+    val shifts = Seq(shift1)
+    val weeklyRota = WeeklyRota(shifts)
+
+    val doctor = Doctor("John Smith", 100, new ListBuffer[Shift]())
+    val doctors = new ListBuffer[Doctor]()
+    doctors += doctor
+
+    // When:
+    allocateWeekdayNormalShifts(Seq(weeklyRota), doctors)
+
+    // Then:
+    doctors(0).shifts should have size (1)
+    doctors(0).shifts(0) should equal(shift1)
+  }
+}
